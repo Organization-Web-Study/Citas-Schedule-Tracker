85acba06cc19f598b7489aafab42466b0e9e235c
orlandoc0107 <orlandocardenas0107@gmail.com>
2024-05-09 14:48:55 -0300

    port 3005

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -15,5 +15,6 @@ async function bootstrap() {
   SwaggerModule.setup('', app, document);
   //cambiar el puto de escucha si estas usando tambien el frontend
   await app.listen(3005);
+  console.log(`Nest application is running on: http://localhost:3005`);
 }
 bootstrap();
